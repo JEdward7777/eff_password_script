8ab6fe1c0661ce2c56f1607c8c6754440cf21ca9
Joshua Lansford <Joshua.Lansford+git@laserlinc.com>
2020-04-16 16:33:26 -0400

    Added clipboard support and fake random support.

--- make_password.py
+++ make_password.py
@@ -1,14 +1,38 @@
 import fileinput
-print( "Please provide random 30 dice:" )
-print( "https://www.random.org/dice/" )
+import os
+import platform
+import secrets
 
-line = next(fileinput.input())
+def addToClipBoard(text):
+  if platform.system() == "Linux":
+    command = 'echo -n "{}" | xclip -selection c'.format(text.strip())
+  else:
+    command = 'echo | set /p nul=' + text.strip() + '| clip'
+  os.system(command)
 
-line = line.replace( " ", "" )
+print( "Is pseudorandomization ok? (yes/no)" )
+print( "> ", end='')
+
+fin = fileinput.input()
+
+pseudorandom = next(fin).lower().startswith( "y" )
+
+if pseudorandom:
+  line = ""
+  for _ in range(30):
+    line += str( secrets.randbelow(6)+1 )
+else:
+  print( "Please provide random 30 dice:" )
+  print( "https://www.random.org/dice/" )
+  line = next(fin)
+  line = line.replace( " ", "" ).strip()
+
+print( "using {} numbers".format( len(line) ) )
+print( line )
 
 result = ""
 
-while( len(line) > 5 ):
+while( len(line) >= 5 ):
   piece = line[:5]
   line = line[5:]
 
@@ -20,3 +44,9 @@ while( len(line) > 5 ):
 	 
 
 print( "Password:" + result )
+
+
+addToClipBoard(result)
+
+
+print( "The password is on your clipboard" )
